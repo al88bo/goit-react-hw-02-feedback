df928bb1296dbd4b03e48f1cd233d3f052083de6
Oleksii Bohoslavskyi <al88bo@gmail.com>
2023-12-30 16:30:23 +0200

    fix

--- src/components/FeedbackOptions/FeedbackOptions.jsx
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -1,14 +1,11 @@
-const FeedbackOptions = ({ options, onLeaveFeedback }) => {
-  const [good, neutral, bad] = options.map(
-    str => (str = str.charAt(0).toUpperCase() + str.slice(1))
-  ); // or {text-transform: capitalize} - in css
-  return (
-    <div onClick={onLeaveFeedback}>
-      <button>{good}</button>
-      <button>{neutral}</button>
-      <button>{bad}</button>
-    </div>
-  );
-};
+const FeedbackOptions = ({ options, onLeaveFeedback }) => (
+  <div>
+    {options.map(option => (
+      <button key={option} type="button" onClick={onLeaveFeedback}>
+        {option}
+      </button>
+    ))}
+  </div>
+);
 
 export { FeedbackOptions };
